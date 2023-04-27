872d0e88c7de73c93a18295adeef6ac7d8905a32
Leonardo Cisternas Albornoz <leonardocisternasalbornoz@Leonardos-MacBook-Pro.local>
2023-04-27 20:33:34 +0200

    Borrando tareas pero aun con bug

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let montoPresupuesto = 0;
 function agregarPresupuesto() {
   let presupuesto = document.getElementById('inputPresupuesto');
   montoPresupuesto = presupuesto.value;
-  let html = `<div>${montoPresupuesto}</div>`;
+  let html = `<div>$ ${parseInt(montoPresupuesto).toLocaleString(
+    'es-ES'
+  )}</div>`;
   document.getElementById('montoPresupuesto').innerHTML = html;
   document.getElementById('inputPresupuesto').value = '';
   document.getElementById('saldo').innerHTML = html;
@@ -28,7 +30,9 @@ function agregarGasto() {
     id="${gasto}"
   >
     <div id="nombreGasto" class="col">${gasto}</div>
-    <div id="valorDeGasto${gasto}" class="col">${gastos[gasto]}</div>
+    <div id="valorDeGasto${gasto}" class="col">$ ${parseInt(
+      gastos[gasto]
+    ).toLocaleString('es-ES')}</div>
     <div class="col">
       <img
       id="img${gasto}"
@@ -40,11 +44,13 @@ function agregarGasto() {
     </div>
   </div>`;
     sumaGastos += parseInt(gastos[gasto]);
-    htmlSumaGastos = `<div id="gastoAcumulado">${sumaGastos}</div>`;
+    htmlSumaGastos = `<div id="gastoAcumulado">$ ${sumaGastos.toLocaleString(
+      'es-ES'
+    )}</div>`;
   }
-  htmlSaldo = `<div id="saldoActual">${
+  htmlSaldo = `<div id="saldoActual">$ ${(
     parseInt(montoPresupuesto) - sumaGastos
-  }</div>`;
+  ).toLocaleString('es-ES')}</div>`;
 
   document.getElementById('sumaGastos').innerHTML = htmlSumaGastos;
   document.getElementById('cardGasto').innerHTML = htmlGasto;
@@ -55,7 +61,10 @@ function agregarGasto() {
 }
 
 function eliminarGasto(nombreGasto) {
+  console.log(Object.keys(gastos).length);
+  console.log(nombreGasto);
   delete gastos[nombreGasto];
+  console.log(Object.keys(gastos).length);
   let sumaGastos = 0;
   for (let gasto in gastos) {
     sumaGastos += parseInt(gastos[gasto]);
@@ -63,17 +72,29 @@ function eliminarGasto(nombreGasto) {
   let saldoActual = parseInt(montoPresupuesto) - sumaGastos;
   let htmlGasto = '';
   for (let gasto in gastos) {
+    // if (Object.keys(gastos).length == 1) {
+    //   htmlGasto = ``;
+
+    //   document.getElementById('sumaGastos').textContent = ``;
+    //   document.getElementById('saldo').textContent = ``;
+    // } else {
     htmlGasto += `
       <div class="row border-bottom border-dark-subtle mx-4 d-flex flex-nowrap" id="${gasto}">
         <div class="col">${gasto}</div>
-        <div class="col">${gastos[gasto]}</div>
+        <div class="col">$ ${gastos[gasto].toLocaleString('es-ES')}</div>
         <div class="col">
           <img src="./assets/images/junk.png" alt="trash icon" class="trashImage" onclick="eliminarGasto('${gasto}')"/>
         </div>
       </div>
     `;
   }
+
   document.getElementById('cardGasto').innerHTML = htmlGasto;
-  document.getElementById('gastoAcumulado').innerHTML = sumaGastos;
-  document.getElementById('saldoActual').innerHTML = saldoActual;
+  document.getElementById(
+    'gastoAcumulado'
+  ).innerHTML = `$ ${sumaGastos.toLocaleString('es-ES')}`;
+  document.getElementById(
+    'saldoActual'
+  ).innerHTML = `$ ${saldoActual.toLocaleString('es-ES')}`;
+  // }
 }
